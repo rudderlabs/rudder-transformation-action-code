48f351029c8f51362a385e5bb501f1b6bc60417d
Jayc <mjayachand@gmail.com>
2023-05-28 22:41:21 +0530

    chore: update readme, code examples

--- code/code.js
+++ code/code.js
@@ -1,27 +1,13 @@
 import { getRevenue, getPrice, getProfit } from "getFinanceData";
 import { getCity, getCountry, getStreet } from "getUserAddress";
-export function transformEvent(events) {
+
+export function transformEvent(event, metadata) {
   return {
-    revenue: getRevenue(events.properties),
-    price: getPrice(events.properties),
-    profit: getProfit(events.properties),
-    city: getCity(
-      (events.context &&
-        events.context.traits &&
-        events.context.traits.address) ||
-        {}
-    ),
-    country: getCountry(
-      (events.context &&
-        events.context.traits &&
-        events.context.traits.address) ||
-        {}
-    ),
-    street: getStreet(
-      (events.context &&
-        events.context.traits &&
-        events.context.traits.address) ||
-        {}
-    )
+    revenue: getRevenue(event.properties || {}),
+    price: getPrice(event.properties || {}),
+    profit: getProfit(event.properties || {}),
+    city: getCity(event.context?.address || {}),
+    country: getCountry(event.context?.address || {}),
+    street: getStreet(event.context?.address || {}),
   };
 }
