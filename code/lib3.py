8d48d6130f12e4761e4ffc110fd4bf92bca9f732
Jayc <mjayachand@gmail.com>
2023-05-28 23:33:32 +0530

    success with mismatch outputs

--- code/lib3.py
+++ code/lib3.py
@@ -1,8 +1,8 @@
 def getPrice(finance):
-  return int(finance.get("price", 0))
+  return float(finance.get("price", 0))
 
 def getRevenue(finance):
-  return int(finance.get("revenue", 0))
+  return float(finance.get("revenue", 0))
 
 def getProfit(finance):
   return getPrice(finance) - getRevenue(finance)
